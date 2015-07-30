393c1b1e640f39d2c2e174cb15c78abe518c7fbc
Josef Mixan <josef.mixan@smartersoftware.co.nz>
2015-07-30 15:13:59 +1200

    Added base domain structure with Category and Factor. Added base classes for xml dynamic framework.

---
+++ Domain/Category.cs
@@ -0,0 +1,31 @@
+﻿namespace Domain
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using XmlDynamic;
+
+    public class Category
+    {
+        public Category()
+        {
+            CreatedOn = DateTimeOffset.Now;
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        public DateTimeOffset CreatedOn { get; set; }
+        public DateTimeOffset? DeletedOn { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "xml")]
+        public string BlueprintData { get; set; }
+
+        [NotMapped]
+        public ICollection<FieldDefinition> Fields { get; set; }
+    }
+}
---
+++ Domain/Factor.cs
@@ -0,0 +1,31 @@
+﻿namespace Domain
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using XmlDynamic;
+
+    public class Factor
+    {
+        public Factor()
+        {
+            CreatedOn = DateTimeOffset.Now;
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        public DateTimeOffset CreatedOn { get; set; }
+        public DateTimeOffset? DeletedOn { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "xml")]
+        public string ConstructData { get; set; }
+
+        [NotMapped]
+        public ICollection<FieldValue> Fields { get; set; }
+    }
+}
