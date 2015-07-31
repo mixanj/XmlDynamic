00fc0f5ca1a9f9045167baf79c09ccc8e5abe7c5
Josef Mixan <josef.mixan@smartersoftware.co.nz>
2015-07-31 19:24:55 +1200

    Deserialization of Blueprint

--- XmlDynamic/Serializer.cs
+++ XmlDynamic/Serializer.cs
@@ -10,6 +10,8 @@
         private const string XElement_Field = "field";
         private const string XElement_Value = "value";
         private const string XAttribute_Id = "id";
+        private const string XAttribute_Name = "name";
+        private const string XAttribute_Type = "type";
 
         /* Construct xml structure to deserialize
          * <construct>
@@ -41,9 +43,35 @@
             return fieldValue;
         }
 
+        /* Blueprint xml structure to deserialize
+         * <blueprint>
+         *	<field id="" name="" type="System.Int32">
+         *		<metadata></metadata>
+         *	</field>
+         *	<field id="" name="" type="System.String">
+         *		<metadata></metadata>
+         *	</field>
+         * </blueprint>
+         */
         public static ICollection<FieldDefinition> DeserializeBlueprint(string blueprintData)
         {
-            throw new NotImplementedException();
+            var fields = new List<FieldDefinition>();
+            var blueprint = XElement.Parse(blueprintData).Elements(XElement_Field);
+            fields.AddRange(blueprint.Select(DeserializeFieldDef));
+            return fields;
+        }
+
+        private static FieldDefinition DeserializeFieldDef(XElement field)
+        {
+            var id = Guid.Parse(field.Attribute(XAttribute_Id).Value);
+            var name = field.Attribute(XAttribute_Name).Value;
+            var typeNme = field.Attribute(XAttribute_Type).Value;
+            return new FieldDefinition
+            {
+                Id = id,
+                Name = name,
+                FieldType = Type.GetType(typeNme)
+            };
         }
     }
 }
