20b6dad8ae65f79dd4257443a4edde2b0c75e695
Josef Mixan <josef.mixan@smartersoftware.co.nz>
2015-07-31 19:44:55 +1200

    Console program to test xml blueprint and construct parsing into dynamic generic object model.

--- XmlDynamicConsole/Program.cs
+++ XmlDynamicConsole/Program.cs
@@ -1,9 +1,34 @@
 ﻿namespace XmlDynamicConsole
 {
+    using System;
+    using System.IO;
+    using Domain;
+    using XmlDynamic;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var blueprint = File.ReadAllText("blueprint.xml");
+            var construct = File.ReadAllText("construct.xml");
+
+            var category = new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = "Category 1",
+                BlueprintData = blueprint
+            };
+            var factor = new Factor
+            {
+                Id = Guid.NewGuid(),
+                Name = "Factor 1",
+                Category = category,
+                ConstructData = construct
+            };
+
+            var customFactorFields = factor.Fields;
+
+            Console.ReadKey();
         }
     }
 }
